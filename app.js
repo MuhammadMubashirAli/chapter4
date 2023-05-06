2d8afe4b50bc2b128f601516edffe1646cba4193
MuhammadMubashirAli <121821846+MuhammadMubashirAli@users.noreply.github.com>
2023-05-06 16:32:48 +0500

    assignment4

---
+++ app.js
@@ -0,0 +1,9 @@
+var userName,age,country;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// legal names
+ var myVar, num123, student_age, $, username;
+
+// illegal names
+// var 12num, my-name , const, function, true
+
